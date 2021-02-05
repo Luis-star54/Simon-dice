c96d8ea72c66f0688bd3d7cbe6ef9891d1eccdce
luis <luisortegaa4222@gmail.com>
2021-02-05 10:41:37 -0500

    Finalizacion de incio de sesion y maquetacion del juego

---
+++ main.js
@@ -0,0 +1,23 @@
+const celeste = document.getElementById('celeste')
+const violeta = document.getElementById('violeta')
+const naranja = document.getElementById('naranja')
+const verde = document.getElementById('verde')
+const btnEmpezar = document.getElementById('btnEmpezar')
+
+class Juego {
+  constructor() {
+    this.inicializar()
+    this.generarSecuencia()
+  }
+
+  inicializar() {
+    btnEmpezar.classList.add('hide')
+  }
+  generarSecuencia(){
+    this.secuencia =  new Array(10).fill(0).map(n => Math.random()* 4) //asi se 
+  }
+}
+
+function empezarJuego() {
+  var juego = new Juego()
+}
